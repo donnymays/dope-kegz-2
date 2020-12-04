80808e4913e7200eea345b585d2b6afa11103278
donnymays <dtmays2@gmail.com>
2020-12-04 10:50:27 -0800

    passing test for selecting and deselecting kegs in selected keg reducer

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,5 +1,6 @@
 import selectedKegReducer from '../../reducers/selected-keg-reducer';
-import * as a from './../../actions/ActionTypes'
+import * as a from './../../actions/ActionTypes';
+import * as actions from './../../actions/index';
 
 describe('selectedKegReducer', () => {
   const testKeg = {
@@ -13,8 +14,31 @@ describe('selectedKegReducer', () => {
 
   test('Should return default state if no action type is recognized', () => {
     expect(selectedKegReducer(null, { type: null })).toEqual(null);
-  })
+  });
 
+  test('Should successfully return selected keg data', () => {
+    const action = actions.selectKeg(testKeg);
+    expect(selectedKegReducer(null, action)).toEqual({
+      name: "testName",
+      brand: "testBrand",
+      price: 100,
+      abv: 5,
+      pints: 124,
+      id: 1 
+    });
+  });
+
+  test('Should successfully deselct a keg', () => {
+    const action = actions.deselectKeg();
+    expect(selectedKegReducer({
+      name: "testName",
+      brand: "testBrand",
+      price: 100,
+      abv: 5,
+      pints: 124,
+      id: 1
+    }, action)).toEqual(null);
+  });
 });
 
 
