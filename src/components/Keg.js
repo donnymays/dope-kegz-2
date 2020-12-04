245f4fa249a56bc7b0c5247cd5a9de69e2810674
donnymays <dtmays2@gmail.com>
2020-12-04 14:14:27 -0800

    add background image

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -2,22 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Keg(props){
-  let typeImage;
-  if (props.types === 'paleAle') {
-    typeImage = '<div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>'
-  } else if (props.types === 'lager') {
-    typeImage = 'public/images/lager.png';
-  } else if (props.types === 'amber') {
-    typeImage = 'public/images/amber.png';
-  } else {
-    typeImage = 'public/images/stout.png';
-  }
+  // let typeImage;
+  // if (props.types === 'paleAle') {
+  //   typeImage = '<div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>'
+  // } else if (props.types === 'lager') {
+  //   typeImage = '<div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
+  // } else if (props.types === 'amber') {
+  //   typeImage = '<div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
+  // } else {
+  //   typeImage = '<div>Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
+  // }
   
   return (
     <React.Fragment>
       <div className='keg' onClick = {() => props.whenKegClicked(props.id)}>
         <h3>{props.name}</h3>
-        {typeImage}
+        {/* {typeImage} */}
         <p>Pints Left: {props.pints}</p>
         <hr/>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers'; 
 
+
 const store = createStore(rootReducer);
 console.log(store.getState());
 
